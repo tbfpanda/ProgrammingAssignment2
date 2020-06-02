cb1eff62a42890745a23ec71555479ca41952f9f
tbfpanda <vyomeshtewari@gmail.com>
2020-06-02 12:51:17 +0530

    Final commit for assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    invmatrix <- NULL #setting initial value of inverse matrix to NULL
+    setmatrix <- function(y){
+        
+        x <<- y # making function to set the value of matrix
+        invmatrix <<- NULL
+    }
+    getmatrix <- function() x #function to get the matrix
+    setinverse <- function(value) invmatrix<<-value #function to set inverse of matrix
+    getinverse <- function() invmatrix #function to get inverse of matrix
+    list(getmatrix=getmatrix, setmatrix=setmatrix, getinverse=getinverse,setinverse=setinverse)
+    #returns list of all the functions
 
 }
 
@@ -11,5 +22,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    invmatrix <- x$getinverse()   # checking if inverse matrix is cached
+    if (!is.null(invmatrix)){
+        return(invmatrix)
+    }
+    else {
+        matrix <- x$getmatrix()
+        invmatrix <<- solve(matrix)
+        x$setinverse(invmatrix)  #evaluated in case inverse matrix is not cached
+        return(invmatrix)
+    }
+       
 }
